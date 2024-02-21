176e02efed4dac33b5aec6d6f4ea3ad202638bc2
Aaron de los Santos <aaronjournalist@hotmail.com>
2024-02-21 16:48:53 +0100

    fix: img footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,9 @@ const Footer = () => {
         <section className='text-center '>
             <p>© 2023 DrinkQuest®</p>
         </section>
-        <section className="social-link flex justify-center">
-          <a className="flex items-center" href='https://github.com/aaron25484' target='_blank'>Aaron de los Santos<img className="github size-4 ml-3" src="src/assets/img/github.png" alt="github logo" ></img> </a>   
+        <section className="social-link flex justify-center items-center">
+          <a className="flex items-center" href='https://github.com/aaron25484' target='_blank'>Aaron de los Santos</a>
+          <img className="github size-4 ml-3" src="src/assets/img/github.png" alt="github logo" ></img>   
         </section>
     </footer>
 )
